e563f174bfe7e03c3c43a80b3719331ceef69506
Izahe <70613319+Izahe@users.noreply.github.com>
2021-09-06 18:59:20 -0400

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -2,66 +2,24 @@
 
 const e = React.createElement;
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
   }
 
   render() {
-    const status = 'Next player: X';
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
     );
   }
 }
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(Game /), domContainer);
-
-
+ReactDOM.render(e(LikeButton), domContainer);
