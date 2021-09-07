d5b5e21e471b4357694ee83c702a8b3dbdedf097
Izahe <70613319+Izahe@users.noreply.github.com>
2021-09-07 16:44:54 -0400

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -58,6 +58,6 @@ class Game extends React.Component {
 // ========================================
 
 const domContainer = document.querySelector('#tic_container');
-ReactDOM.render(Game /, domContainer);
+ReactDOM.render(<Game />, domContainer);
 
 
